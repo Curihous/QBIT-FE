c15253d3299a6c8c0375dab3587cb735b30ff4ae
jud1thdev <8w8u8@naver.com>
2025-10-24 16:47:54 +0900

    fix: bottom 값 다시 0으로 조정

--- packages/shared/lib/screens/home/home_screen.dart
+++ packages/shared/lib/screens/home/home_screen.dart
@@ -36,7 +36,7 @@ class _HomeScreenState extends State<HomeScreen> {
           Positioned(
             left: 0,
             right: 0,
-            bottom: MediaQuery.of(context).padding.bottom,
+            bottom: 0,
             child: CustomBottomNavigationBar(
               currentIndex: _currentIndex,
               onTap: (index) {
